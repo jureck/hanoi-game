bd16369f6bca64763321ccf091573425e13e609e
jureck <prawiepro1337@gmail.com>
2021-02-21 21:45:09 +0100

    Updates

---
+++ precache-manifest.63e49e03764d1d6cf64c136844ba38f7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b61b6932bb2b453cc81203b96d718687",
+    "url": "/hanoi-game/index.html"
+  },
+  {
+    "revision": "8996c256ed2b970145c4",
+    "url": "/hanoi-game/static/css/main.4b13e4dc.chunk.css"
+  },
+  {
+    "revision": "874a36ca900947402777",
+    "url": "/hanoi-game/static/js/2.e6f30f09.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/hanoi-game/static/js/2.e6f30f09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8996c256ed2b970145c4",
+    "url": "/hanoi-game/static/js/main.58a9a728.chunk.js"
+  },
+  {
+    "revision": "3200a8ef350e84535d5a",
+    "url": "/hanoi-game/static/js/runtime-main.7596f814.js"
+  },
+  {
+    "revision": "39bbf5e2255cb0c46d0c788da3cd5b3c",
+    "url": "/hanoi-game/static/media/restart.39bbf5e2.svg"
+  },
+  {
+    "revision": "81c1c77c162ba7a28936be40f47b6138",
+    "url": "/hanoi-game/static/media/settings.81c1c77c.svg"
+  }
+]);
